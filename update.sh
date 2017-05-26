e96f5a7e62af9313548fad387a49aa8ea6ac451c
Luiz Nickel <luizaugustonickel@gmail.com>
2017-05-26 19:07:33 -0300

    Adding pycharm update verification

--- update.sh
+++ update.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-NUMBER_OF_FILES=`ls -1 PhpStorm-*.tar.gz 2> /dev/null | wc -l`
+NUMBER_OF_FILES_PS=`ls -1 PhpStorm-*.tar.gz 2> /dev/null | wc -l`
+NUMBER_OF_FILES_PC=`ls -1 PyCharm-*.tar.gz 2> /dev/null | wc -l`
 
-if [ "$NUMBER_OF_FILES" == "0" ]
+if [ "$NUMBER_OF_FILES_PS" == "0" ]
 then
 	echo "There are no PhpStorm source packages in this directory."
 	echo "Please go to https://www.jetbrains.com/phpstorm/download/index.html to download the .tar.gz file and try again."
 	exit
 fi
 
+if [ $NUMBER_OF_FILES_PC == "0" ]
+then
+	echo "There are no PyCharm source packages in this directory."
+	echo "Please go to https://www.jetbrains.com/pycharm/download/index.html to download the .tar.gz file and try again."
+	exit
+fi
 
-if [ "$NUMBER_OF_FILES" != "1" ]
+if [ "$NUMBER_OF_FILES_PS" != "1" ]
 then
 	echo "There are multiple PhpStorm source packages in this directory."
+	echo "Please remove old .tar.gz files and try again."
+	exit
+fi
+
+
+if [ "$NUMBER_OF_FILES_PS" != "1" ]
+then
+	echo "There are multiple PyCharm source packages in this directory."
 	echo "Please remove old .tar.gz files and try again."
 	exit
 fi
